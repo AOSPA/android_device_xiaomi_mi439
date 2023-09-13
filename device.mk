37cc0698cc379187bbc3623551fbcfe762f31dc7
me-cafebabe <me.cafebabe@gmail.com>
2023-09-14 04:49:01 +0800

    mi439: Fix media_profiles.xml copy rule
    Change-Id: Ie879206bda1c48ced1cb66bcabb6374c14671734

--- device.mk
+++ device.mk
@@ -184,7 +184,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_8937.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_8937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_8956.xml:system/etc/media_profiles_8956.xml \
